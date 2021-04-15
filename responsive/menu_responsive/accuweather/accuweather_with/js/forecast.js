e1c6885d17d00b936b131057eeb98ed5e4f8378e
BaekYuseon <cofls0518@gmail.com>
2021-04-15 17:40:46 +0900

    4.15 accuweather 5:40pm whfflek

--- responsive/menu_responsive/accuweather/accuweather_with/js/forecast.js
+++ responsive/menu_responsive/accuweather/accuweather_with/js/forecast.js
@@ -1,24 +1,24 @@
 /* 객체 선언 */
 function Forecast() {
+    this.url = "http://api.openweathermap.org/data/2.5/weather"; 
+    this.url += "?units=" + "metric"; //처음은 ? 그 다음부터는 &
+    this.url += "&lang=" + "kr";
+    this.url += "&APPID=" + "d3737381ef6ababa2bd02c9677d79367"; //본인의 API key
+    this.url += "&q=";
 }
 
-/* 현재 날씨 얻어오기 */
+/* 현재 날씨의 모든 정보 얻어오기 */
 
 Forecast.prototype.getCurrentWeather = function(city) {
     var dataObj;
-    var url = "http://api.openweathermap.org/data/2.5/weather";
-        url += "?q=" + city;
-        url += "&units=" + "metric";
-        url += "&lang=" + "kr";
-        url += "&APPID=" + "d3737381ef6ababa2bd02c9677d79367"; //본인의 API key
-
+    var openWeatherAPI = this.url; //q가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
+    
     $.ajax({
         type:"GET",
-        url:url,
+        url:openWeatherAPI += city,
         dataType:"json",
-        async: false, //동기형식으로 지정(data값을 return해주기)
+        async: false, //동기형식으로 지정(data값을 return해주기 위해)
         success:function(data) {
-            console.log("현재온도: " + data.main.temp);
             dataObj = data;
         },
         error:function(request,status,error) { //에러떴을때를 대비하여
@@ -33,8 +33,34 @@ Forecast.prototype.getCurrentWeather = function(city) {
 
 /* 내일 날씨 얻어오기 */
 
-Forecast.prototype.getTomarrowWeather = function() {
+Forecast.prototype.getTomorrowWeather = function() {
     //...
+};
+
+/* 현재 날씨 온도 얻어오기 */
+
+Forecast.prototype.getCurrentTemp = function(city) {
+    var temp;
+    var openWeatherAPI = this.url;//city가 게속 붙으므로 url 초기화를 위해 반드시 넣어준다.
+
+    $.ajax({
+        type:"GET",
+        url:openWeatherAPI += city, 
+        dataType:"json",
+        async: false, // return해주기 위해
+        success:function(data) {
+            temp = Math.floor(data.main.temp); //소수점 버림
+        },
+        error:function(request,status,error) { //에러떴을때를 대비하여
+            console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
+        },
+        complete:function(data,textStatus) {
+            //작업 완료 후 처리
+
+        }
+    });
+
+    return temp;
 }
 
 /* Forecast.prototype.함수명 = function() */
--- responsive/menu_responsive/accuweather/accuweather_with/js/main.js
+++ responsive/menu_responsive/accuweather/accuweather_with/js/main.js
@@ -6,7 +6,10 @@ map.getKakaoMap();
 /* 날씨 */
 
 var forecast = new Forecast();
-console.log(forecast.getCurrentWeather("incheon")); //지역 날씨 호출
-console.log(forecast.getCurrentWeather("seoul")); //지역 날씨 호출
-console.log(forecast.getCurrentWeather("gwangju")); //지역 날씨 호출
-console.log(forecast.getCurrentWeather("jeju")); //지역 날씨 호출
+var cityList = ["seoul", "incheon", "busan", "gwangju", "jeju", "jeonju"];
+
+$(function(){
+    $(".temp").each(function(i) {
+        $(this).text(forecast.getCurrentTemp(cityList[i]) + "°C");
+    });
+});
